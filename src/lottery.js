9e399f6392d9751dcdc5e096bbeddb4cda21d86a
Lokesh Mishra <4man.mishra@gmail.com>
2021-05-10 00:05:18 +0530

    inital commit

---
+++ src/lottery.js
@@ -0,0 +1,8 @@
+import web3 from './web3.js'
+
+const address = '0x4eCe73C24851675E0a21c8BB1f7f73cE22EEe91d'
+
+const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"enter","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"getPlayers","outputs":[{"internalType":"address[]","name":"","type":"address[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"manager","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"pickWinner","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"players","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}];
+ 
+export default new web3.eth.Contract(abi, address);  //exporting a complete copy (not the original contract) of our SC
+
